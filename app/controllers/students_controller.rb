2324dc46ff8749ade520490c5d348f6c4cd29aaa
castagnaTM <castagna.tm@gmail.com>
2019-09-20 15:42:25 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(params.require(:student).permit(:first_name, :last_name)) 
+    @student.created_at = Time.now
+    @student.updated_at = Time.now
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
